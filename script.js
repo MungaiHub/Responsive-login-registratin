8258eb18687477aba72e04b47e99ad7f03b4c29c
mungaiHub <amosmungai085@gmail.com>
2025-07-08 18:20:08 +0300

    update: fixed register form spacing and improved popup transitions

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnpopup=document.querySelector('.popup-login');
+const cover_box=document.querySelector('.cover-box');
+const loginLink=document.querySelector('.login-link');
+const registerLink=document.querySelector('.register-link');
+const iconClose=document.querySelector('.icon-close');
+
+function activateCoverBox(){
+    cover_box.classList.add('active');
+}
+
+function deactivateCoverBox(){
+    cover_box.classList.remove('active');
+}
+
+function activatePopup(){
+    cover_box.classList.add('active-popup');
+}
+
+function deactivateCoverPopup(){
+    cover_box.classList.remove('active-popup');
+}
+registerLink.addEventListener('click', activateCoverBox);
+loginLink.addEventListener('click',deactivateCoverBox);
+btnpopup.addEventListener('click',activatePopup)
+iconClose.addEventListener('click',deactivateCoverPopup);
+
